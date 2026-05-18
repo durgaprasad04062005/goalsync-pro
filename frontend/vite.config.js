c97946b254e9cb99793f79fef748170989e35c98
GL Durga Prasad <gldurgaprasad2005@gmail.com>
2026-05-18 22:23:23 +0530

    Remove node_modules

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,4 +18,19 @@ export default defineConfig({
       '@': '/src',
     },
   },
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor':   ['react', 'react-dom', 'react-router-dom'],
+          'redux-vendor':   ['@reduxjs/toolkit', 'react-redux'],
+          'ui-vendor':      ['framer-motion', '@headlessui/react', '@heroicons/react'],
+          'chart-vendor':   ['recharts'],
+          'utils-vendor':   ['axios', 'date-fns', 'react-hot-toast'],
+        },
+      },
+    },
+  },
 });
